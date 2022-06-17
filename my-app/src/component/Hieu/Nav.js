18b1d9acc37df39b01514da8aa5213c9e1130308
NguyenMinhHieuIT <hieukd01yc@gmail.com>
2022-06-18 02:26:49 +0700

    Hoan thanh nav

--- my-app/src/component/Hieu/Nav.js
+++ my-app/src/component/Hieu/Nav.js
@@ -1,10 +1,12 @@
 import react from "react";
+import logo from "../../img/Huawei-logo.jpg";
+import '../../css/style.css'
 function Nav() {
     return (
         <>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#"><img src="//www-file.huawei.com/-/media/corporate/images/home/logo/huawei_logo.png" style={{height:'auto', width: '115px' , marginTop:'5px'}}/></a>
+                    <a class="navbar-brand" href="#"><img src={logo} style={{ height: 'auto', width: '115px', marginTop: '5px' }} /></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -14,7 +16,7 @@ function Nav() {
                                 <a class="nav-link active" aria-current="page" href="#"><b>Điện thoại</b></a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" href="#"><b>Máy tính xách tay</b></a>    
+                                <a class="nav-link active" href="#"><b>Máy tính xách tay</b></a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true"><b>Máy tính bảng</b></a>
@@ -37,12 +39,26 @@ function Nav() {
                             <li class="nav-item">
                                 <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true"><b>EMUI</b></a>
                             </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true"><b>Hỗ trợ</b></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true"><b>Nhà bán lẻ</b></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true"><b><i class="fa-solid fa-magnifying-glass mt-2" style={{ marginLeft:'50px' }}></i></b></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true"><b><i class="fa-solid fa-user mt-2" style={{ marginLeft:'50px' }}></i></b></a>
+                            </li>                          
                         </ul>
 
-                        <form class="d-flex">
-                            <i class="fa-solid fa-magnifying-glass bg-dark"></i>
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                        </form>
+
+                    
+                            
+                      
+
+
                     </div>
                 </div>
             </nav>
