933292d01f8fea2ac8530c88958b2f9fe073fcec
quannike68 <89019444+quannike68@users.noreply.github.com>
2022-06-21 02:54:09 +0700

    Card

--- my-app/src/component/Quan/Main.js
+++ my-app/src/component/Quan/Main.js
@@ -2,11 +2,13 @@ import React from "react";
 import Nav from "../Hieu/Nav";
 import Footer from "../Hieu/Footer";
 import Slider from "../Quan/Slider";
+import Card from "../Quan/Card";
 function Main() {
   return (
     <div>
       <Nav />
       <Slider />
+      <Card/>
       <Footer />
     </div>
   );
