34731535c97a38e48e87f7b42d45d094e64214f4
quannike68 <89019444+quannike68@users.noreply.github.com>
2022-06-21 02:28:38 +0700

    Slider

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,12 @@ import Phone_main from "./component/Hieu/Phone_main";
 import Product from "./component/Hieu/Product";
 import "./App.css";
 import Main from "./component/Quan/Main";
+import Slider from "./component/Quan/Slider";
 function App() {
   return (
     <>
-      <Main />
+      {/* <Main /> */}
+      {/* <Slider /> */}
       {/* <Phone_main /> */}
       {/* <Product/> */}
     </>
--- my-app/src/App_hieu.js
+++ my-app/src/App_hieu.js
@@ -1,18 +1,20 @@
-import Nmain from './component/Nhan/Nmain';
-import Product_nhan from './component/Nhan/Product_nhan';
-import Footer from '../src/component/Hieu/Footer';
-import Dangnhap from '../src/component/Nhan/Dangnhap'
-import Nav from './component/Hieu/Nav';
-import Product from './component/Hieu/Product';
-import './App.css';
+import Nmain from "./component/Nhan/Nmain";
+import Product_nhan from "./component/Nhan/Product_nhan";
+import Footer from "../src/component/Hieu/Footer";
+import Dangnhap from "../src/component/Nhan/Dangnhap";
+import Nav from "./component/Hieu/Nav";
+import Product from "./component/Hieu/Product";
+import "./App.css";
+import Slider from "./component/Quan/Slider";
 
 function App_hieu() {
   return (
     <>
-    <Nav/>
-   <Product/>
-    <Footer/> 
-  </>
+      <Nav />
+      {/* <Product/> */}
+      <Slider/>
+      <Footer />
+    </>
   );
 }
 
